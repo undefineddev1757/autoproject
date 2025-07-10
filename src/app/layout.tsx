47a6151b1a914619e5b39437ed3f50b0bee44edc
seregaaa228 <shreker240@gmail.com>
2025-07-10 11:44:13 +0300

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ClientBody from "./ClientBody";
 
@@ -29,6 +30,21 @@ export default function RootLayout({
       lang="en"
       className={`scroll-smooth ${geistSans.variable} ${geistMono.variable}`}
     >
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6N2GKSWNMK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6N2GKSWNMK');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning className="antialiased">
         <ClientBody>{children}</ClientBody>
       </body>
