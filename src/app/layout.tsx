86ba9e82aeb66661afa75296779b7c3d85148e31
seregaaa228 <shreker240@gmail.com>
2025-09-02 12:20:22 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import YandexMetrikaInitializer from "@/components/yandex-metrika/YandexMetrikaInitializer";
-import YandexMetrikaContainer from "@/components/yandex-metrika/YandexMetrikaContainer";
-import { YM_COUNTER_ID, analyticsEnabled } from "@/components/yandex-metrika/constants";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,14 +27,56 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
-        <YandexMetrikaInitializer
-          id={YM_COUNTER_ID}
-          initParameters={{ ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", accurateTrackBounce: true, trackLinks: true, defer: true }}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              console.info('[YM] injecting loader...');
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              console.info('[YM] loader inserted, waiting for ym...');
+
+              ym(103969698, "init", {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+              console.info('[YM] init called with counter 103969698');
+
+              (function(){
+                var tryCount = 0;
+                var interval = setInterval(function(){
+                  tryCount++;
+                  if (typeof ym === 'function') {
+                    console.info('[YM] ready, sending hit...');
+                    try {
+                      var url = location.pathname + location.search + location.hash;
+                      ym(103969698, 'hit', url);
+                      console.info('[YM] hit sent:', url);
+                    } catch (e) {
+                      console.warn('[YM] hit error:', e);
+                    }
+                    clearInterval(interval);
+                  } else if (tryCount >= 50) { // ~5s
+                    console.warn('[YM] not ready after 5s');
+                    clearInterval(interval);
+                  }
+                }, 100);
+              })();
+            `,
+          }}
         />
       </head>
       <body>
         {children}
-        <YandexMetrikaContainer enabled={analyticsEnabled} />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103969698" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
--- src/components/yandex-metrika/YandexMetrikaInitializer.tsx
+++ src/components/yandex-metrika/YandexMetrikaInitializer.tsx
@@ -20,7 +20,7 @@ const YandexMetrikaInitializer: React.FC<Props> = ({ id, initParameters }) => {
   m[i].l=1*new Date();
   for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${id}', 'ym');
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
 ym(${id}, 'init', ${JSON.stringify(initParameters)});`}
       </Script>
