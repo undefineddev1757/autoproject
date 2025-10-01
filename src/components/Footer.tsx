9fbc9e363cf541934a14ba7c4e15b19488982cd6
seregaaa228 <shreker240@gmail.com>
2025-10-01 14:09:45 +0300

    фон2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,10 +21,10 @@ export function Footer() {
           {/* Contact & Social */}
           <div className="flex items-center space-x-6">
             <a
-              href="tel:+74999559671"
+              href="tel:+74999559654"
               className="text-white hover:text-blue-400 font-medium transition-colors duration-300"
             >
-              +7 (499) 955-96-71
+              +7 (499) 955-96-54
             </a>
             <div className="flex space-x-3">
               <a
